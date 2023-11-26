46ea9278a0919884dd7abd252480ae18f7bc82c3
ALONUCLEAR <alonrazka58@gmail.com>
2023-11-26 07:25:30 +0200

    fix: remove unnecessary library

--- japanese-vocab-website/src/app/Utils/general.utils.ts
+++ japanese-vocab-website/src/app/Utils/general.utils.ts
@@ -1,5 +1,3 @@
-import { toWords } from "number-to-words";
-
 /**A function to convert big integers(in string form) to their number counterpart.
  * Since big integers are bigger than numbers can be(otherwise we would've used numbers),
  * This function gets a dotPlacement variable, whic is the power of 10 that we divide by,
@@ -18,6 +16,9 @@ export function bigIntStrToNumber(bigIntStr: string, dotPlacement: number = 3): 
 
 const VOWELS = [...'aiueo'];
 
+/**All the numbers that start with vowels up to 100 */
+const NUMBERS_THAT_START_WITH_VOWELS = [1, 8, 11, 18, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89]
+
 export const calcAge = (today: Date, birth: Date): number => {
   const millisecondsPerYear = 365.25 * 24 * 60 * 60 * 1000; // Average milliseconds in a year
   const msDiff = today.getTime() - birth.getTime();
@@ -25,7 +26,7 @@ export const calcAge = (today: Date, birth: Date): number => {
   return Math.floor(msDiff / millisecondsPerYear);
 };
 
-export const getPrefix = (num: number): string => VOWELS.includes(toWords(num)[0]) ? 'an' : 'a';
+export const getPrefix = (num: number): string => NUMBERS_THAT_START_WITH_VOWELS.includes(num) ? 'an' : 'a';
 
 export const randInt = (min: number = 0, max: number = 10) => min + Math.floor(Math.random() * (max - min));
 
